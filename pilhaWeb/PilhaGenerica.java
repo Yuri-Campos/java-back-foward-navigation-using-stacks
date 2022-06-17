b71a5b5bb5138b787e2c442e1faef1b6ecf5a71f
Yuri Campos <yuri.g.s.campos@gmail.com>
2022-06-16 23:05:32 -0300

    complete pilhaGenerica methods

--- pilhaWeb/PilhaGenerica.java
+++ pilhaWeb/PilhaGenerica.java
@@ -4,28 +4,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PilhaGenerica<TIPO> {
-	//private int n;
+	private int n;
 	private List<TIPO> vetor;
 	private int topo;
 	public PilhaGenerica(int tam)
 	{
-		//n = tam;
+		n = tam;
 		vetor = new ArrayList<TIPO>(tam);
 		topo = -1;
+	}
+	
+	
+	public boolean peekTopo()
+	{
+		if(!isVazia())
+		{
+			System.out.println("Elemento do topo: " + vetor.get(topo));
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isVazia()
+	{
+		return topo == -1;
+	}
+	
+	public boolean isCheia()
+	{
+		return topo == n - 1;
 	}
 	
 	public TIPO pop()
 	{
 		TIPO elem;
-		
+		if(!isVazia())
+		{
 		elem = vetor.get(topo--);
 		return elem;
+		}
+		System.out.println("Pop falhou. Pilha vazia");
+		return null;
 	}
 	
 	
 	public boolean push(TIPO elem)
 	{
+		if(!isCheia())
+		{
 		vetor.add(++topo, elem);
 		return true;
+		}
+		System.out.println("Push nao funcionou. Pilha cheia");
+		return false;
 	}
 }
